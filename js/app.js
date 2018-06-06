81c9f5d0485b50f2e85588fb573ca9f2e1fdcd1c
Chittipong <jittipong.m@gmail.com>
2018-06-06 23:34:55 +0700

    Manage Database

--- js/app.js
+++ js/app.js
@@ -3,12 +3,54 @@ var app = new Vue({
     data:{
         showAddModal:false,
         showEditModal:false,
-        showDeleteModal:false
+        showDeleteModal:false,
+        errorMessage:"",
+        successMessage:"",
+        users:[],
+        newUser:{username:"",email:"",mobile:""}
     },
     mounted:function(){
-        console.log("aaaaa");
+        console.log("mounted");
+        this.getAllUsers();
     },
     methods:{
+        getAllUsers:function(){
+            axios.get("http://localhost/vue_crud/api.php?action=read")
+            .then(function(response){
+                //console.log(response);
+                if(response.data.error){
+                    app.errorMessage=response.data.message;
+                }else{
+                    app.users=response.data.users;
+                }
+            });
+        },//End***
 
+        saveUser:function(){
+            //console.log(app.newUser);
+            var formData=app.toFormData(app.newUser);
+
+            axios.post("http://localhost/vue_crud/api.php?action=create", formData)
+            .then(function(response){
+                //console.log(response);
+                app.newUser={username:"",email:"",mobile:""}
+                if(response.data.error){
+                    app.errorMessage=response.data.message;
+                }else{
+                    app.getAllUsers();
+                }
+            });
+        },
+        toFormData:function(obj){
+            var form_data=new FormData();
+            for(var key in obj){
+                form_data.append(key,obj[key]);
+            }
+            return form_data;
+        },
+        clearMessage:function(){
+            app.errorMessage="";
+            app.successMessage="";
+        }
     }
 });
